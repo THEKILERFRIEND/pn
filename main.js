c49c35334cc0ee0683db634a1585d4ae0cb9aff2
THEKILERFRIEND <32984678+THEKILERFRIEND@users.noreply.github.com>
2023-12-26 01:29:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,17 +5,11 @@ document.addEventListener("DOMContentLoaded", function() {
   const landingPage = document.getElementById("landing-page");
 
   enterButton.addEventListener("click", function() {
-    // Play background music
     backgroundMusic.play();
 
-    // Display and play the video background
     videoBackground.style.display = "block";
     videoBackground.play();
 
-    // Set disableRemotePlayback to prevent video pause on tab switch
-    videoBackground.setAttribute("disableRemotePlayback", "true");
-
-    // Hide landing page with fade-out effect
     landingPage.style.transition = "opacity 1s";
     landingPage.style.opacity = 0;
 
