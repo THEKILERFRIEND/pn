76e0624b37729305de8a9be4af07e989b2f04016
THEKILERFRIEND <32984678+THEKILERFRIEND@users.noreply.github.com>
2023-12-26 02:02:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   const videoBackground = document.getElementById("video-background");
   const enterButton = document.querySelector(".enter-button");
-  const backgroundMusic = document.getElementById("background-music");
   const landingPage = document.getElementById("landing-page");
 
   enterButton.addEventListener("click", function() {
-    backgroundMusic.play();
-
+    // Show and play the video
     videoBackground.style.display = "block";
     videoBackground.play();
 
+    // Hide landing page with fade-out effect
     landingPage.style.transition = "opacity 1s";
     landingPage.style.opacity = 0;
 
