e2e4b562d531a116db9b630c9c3112f628d82e78
THEKILERFRIEND <32984678+THEKILERFRIEND@users.noreply.github.com>
2023-12-26 01:30:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,16 +5,20 @@ document.addEventListener("DOMContentLoaded", function() {
   const landingPage = document.getElementById("landing-page");
 
   enterButton.addEventListener("click", function() {
+    // Play background music
     backgroundMusic.play();
 
-    videoBackground.style.display = "block";
-    videoBackground.play();
-
+    // Hide landing page with fade-out effect
     landingPage.style.transition = "opacity 1s";
     landingPage.style.opacity = 0;
 
     setTimeout(function () {
       landingPage.style.display = "none";
     }, 1000);
+
+    // Show and play the video
+    videoBackground.style.display = "block";
+    videoBackground.volume = 0.36; // Set the volume to 36% (adjust as needed)
+    videoBackground.play();
   });
 });
